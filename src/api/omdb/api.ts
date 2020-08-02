56b4bef57a57efaa121ef6507b83bc17c63a1892
lukaszgluszek <54551699+lukaszgluszek@users.noreply.github.com>
2020-08-02 18:12:51 +0200

    Use https for omdbapi requests

--- src/api/omdb/api.ts
+++ src/api/omdb/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const key = '9065506b';
 
 const api = axios.create({
-  baseURL: `http://www.omdbapi.com/?apikey=${key}`,
+  baseURL: `https://www.omdbapi.com/?apikey=${key}`,
 });
 
 export default api;
